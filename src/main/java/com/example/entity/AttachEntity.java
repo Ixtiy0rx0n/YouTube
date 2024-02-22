3f83eb06c4b931dd1d4829e3b1176c19e5098c5d
Ixtiyorxon Xabibulloyev <xabibulloyevixtiyorxon@gmail.com>
2024-02-22 15:19:06 +0500

    Done project

--- src/main/java/com/example/entity/AttachEntity.java
+++ src/main/java/com/example/entity/AttachEntity.java
@@ -1,39 +1,39 @@
-package com.example.entity;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
-import java.time.LocalDateTime;
-
-@Setter
-@Getter
-@Entity
-@Table(name = "attach")
-public class AttachEntity {
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    private Integer id;
-
-    @Column(name = "original_name")
-    private String originalName;
-    @Column(name = "size")
-    private Long size;
-
-    @Column(name = "type")
-    private String type;
-
-    @Column(name = "path")
-    private String path;
-
-    @Column(name = "created_date")
-    private LocalDateTime createdDate;
-
-    @Column(name = "visible")
-    private Boolean visible = true;
-
-    @Column(name = "duration", nullable = false)
-    private Integer duration;
-}
+//package com.example.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.hibernate.annotations.GenericGenerator;
+//
+//import java.time.LocalDateTime;
+//
+//@Setter
+//@Getter
+//@Entity
+//@Table(name = "attach")
+//public class AttachEntity {
+//    @Id
+//    @GeneratedValue(generator = "UUID")
+//    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+//    private Integer id;
+//
+//    @Column(name = "original_name")
+//    private String originalName;
+//    @Column(name = "size")
+//    private Long size;
+//
+//    @Column(name = "type")
+//    private String type;
+//
+//    @Column(name = "path")
+//    private String path;
+//
+//    @Column(name = "created_date")
+//    private LocalDateTime createdDate;
+//
+//    @Column(name = "visible")
+//    private Boolean visible = true;
+//
+//    @Column(name = "duration", nullable = false)
+//    private Integer duration;
+//}
